9a603f9d3c29f66a7cd6ef6af9a4f0e4f53d3420
feroline <anacarolina.rrocha@gmail.com>
2024-06-26 18:54:22 -0300

    colocando data-testid em uma const para reutilizar, não é uma refatoração

--- cypress/e2e/add.cy.js
+++ cypress/e2e/add.cy.js
@@ -1,10 +1,10 @@
 describe('Lista TODO', () => {
-
-// TODO:Refatorar 
+ 
   it('Deve guardar o valor de um input como texto', () => {
     cy.visit('http://localhost:3000/')
-    
-    cy.get('input[data-testid="todo-input"]').type('Primeiro item TODO')
-    cy.get('input[data-testid="todo-input"]').should('have.value', 'Primeiro item TODO')
+
+    const input = 'input[data-testid="todo-input"]' 
+    cy.get(input).type('Primeiro item TODO')
+    cy.get(input).should('have.value', 'Primeiro item TODO')
   })
 })
