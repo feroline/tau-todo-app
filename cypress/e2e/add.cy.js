006a1ba0435ee6a83306e8e1e0bd4f5fe3ee0914
feroline <anacarolina.rrocha@gmail.com>
2024-06-26 18:47:08 -0300

    adicionando elemento visível a tela e corrigindo nome do data-testid no cypress

--- cypress/e2e/add.cy.js
+++ cypress/e2e/add.cy.js
@@ -4,7 +4,7 @@ describe('Lista TODO', () => {
   it('Deve guardar o valor de um input como texto', () => {
     cy.visit('http://localhost:3000/')
     
-    cy.get('[data-testid="input-todo"]').type('Primeiro item TODO')
-    cy.get('[data-testid="input-todo"]').should('have.value', 'Primeiro item TODO')
+    cy.get('input[data-testid="todo-input"]').type('Primeiro item TODO')
+    cy.get('input[data-testid="todo-input"]').should('have.value', 'Primeiro item TODO')
   })
 })
