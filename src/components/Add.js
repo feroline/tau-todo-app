45d39706c531e8380b750329615f515624ff2539
feroline <anacarolina.rrocha@gmail.com>
2024-06-26 18:25:49 -0300

    corrigindo erros de name e nome do value

--- src/components/Add.js
+++ src/components/Add.js
@@ -3,16 +3,16 @@ import { useState } from "react";
 export default function Add() {
 
     const [data, setData] = useState({
-        addTodo: ''
-    });
+        add_todo: ''
+    })
 
     const onChange = (evento) => {
         const key = evento.target.name;
         const value = evento.target.value;
-        setData(oldData => ({...oldData, [key]:value }))
+        setData(oldData => ({ ...oldData, [key]: value }))
     }
 
     return (
-        <input data-testid="todo-input" value={data.addTodo} onChange={onChange} name="add-todo" />
+        <input data-testid="todo-input" value={data.add_todo} onChange={onChange} name="add_todo" />
     )
 }
