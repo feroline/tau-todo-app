3a3243139941b24c6c99f4f53eb0b44f25058263
feroline <anacarolina.rrocha@gmail.com>
2024-06-26 19:41:34 -0300

    adicionando eyeApplitools

--- cypress/e2e/add.cy.js
+++ cypress/e2e/add.cy.js
@@ -1,10 +1,32 @@
 describe('Lista TODO', () => {
  
+  beforeEach(() => {
+    cy.eyesOpen({
+      appName: 'tau-todo-app',
+      testName: Cypress.currentTest.title,
+    })
+  })
+
+  afterEach(() => {
+    cy.eyesClose()
+  })
+
   it('Deve guardar o valor de um input como texto', () => {
     cy.visit('http://localhost:3000/')
 
     const input = 'input[data-testid="todo-input"]' 
     cy.get(input).type('Primeiro item TODO')
     cy.get(input).should('have.value', 'Primeiro item TODO')
+  
+    cy.eyesCheckWindow({
+      tag: 'add input', 
+      target: 'region',
+      matchLevel: 'Strict',
+      selector: {
+        type: 'name',
+        selector: 'add_todo'
+      }
+    })
+
   })
 })
