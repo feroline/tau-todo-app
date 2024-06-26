f78ded54ee50e6dfb8cdfd9f07dd88398b0ea369
feroline <anacarolina.rrocha@gmail.com>
2024-06-26 17:17:20 -0300

    adicionando comando de teste para o componente Add, que ainda não existe

--- src/__tests__/add.spec.js
+++ src/__tests__/add.spec.js
@@ -2,3 +2,12 @@ import React from "react";
 import { render, screen } from '@testing-library/react';
 import userEvent from "@testing-library/user-event";
 import '@testing-library/jest-dom';
+
+import Add from '../components/Add'
+
+describe('todo add', () => {
+    test('armazenando valor do input', () => {
+        render(<Add></Add>) // passa o componente que deseja testar
+
+    })
+})
