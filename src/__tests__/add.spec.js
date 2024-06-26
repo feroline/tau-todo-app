f876aa0d1f03461726e1a517486c995645815c19
feroline <anacarolina.rrocha@gmail.com>
2024-06-26 17:51:19 -0300

    adicinando verificação dos testes com jest

--- src/__tests__/add.spec.js
+++ src/__tests__/add.spec.js
@@ -6,8 +6,14 @@ import '@testing-library/jest-dom';
 import Add from '../components/Add'
 
 describe('todo add', () => {
-    test('armazenando valor do input', () => {
+    test('armazenando valor do input', async () => {
         render(<Add />) // passa o componente que deseja testar
 
+        await userEvent.type( // tem os comandos nativos para interagir com a página
+            screen.getByTestId('todo-input') // pega todos os elementos da tela 
+            , 'segundo item todo' // passa o texto que deseja digitar
+        )
+ 
+        expect(screen.getByDisplayValue(/segundo item todo/)).toBeInTheDocument() // verifica se o texto foi digitado
     })
 })
