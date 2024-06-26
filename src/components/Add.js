4affde127880b736119183263f890b8a48a428f0
feroline <anacarolina.rrocha@gmail.com>
2024-06-26 18:09:29 -0300

    adicionando component Add

--- src/components/Add.js
+++ src/components/Add.js
@@ -1,3 +1,18 @@
+import { useState } from "react";
+
 export default function Add() {
-    
+
+    const [data, setData] = useState({
+        addTodo: ''
+    });
+
+    const onChange = (evento) => {
+        const key = evento.target.name;
+        const value = evento.target.value;
+        setData(oldData => ({...oldData, [key]:value }))
+    }
+
+    return (
+        <input data-testid="todo-input" value={data.addTodo} onChange={onChange} name="add-todo" />
+    )
 }
