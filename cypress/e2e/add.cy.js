9b3e676813cce5fe495369fb2f25085aeedc9a7d
feroline <anacarolina.rrocha@gmail.com>
2024-06-26 16:56:07 -0300

    corrigindo https para http

--- cypress/e2e/add.cy.js
+++ cypress/e2e/add.cy.js
@@ -2,7 +2,7 @@ describe('Lista TODO', () => {
 
 // TODO:Refatorar 
   it('Deve guardar o valor de um input como texto', () => {
-    cy.visit('https://localhost:3000/')
+    cy.visit('http://localhost:3000/')
     
     cy.get('[data-testid="input-todo"]').type('Primeiro item TODO')
     cy.get('[data-testid="input-todo"]').should('have.value', 'Primeiro item TODO')
