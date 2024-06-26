6a7a4c98d3eeeaa73bcdc65514199c012f443ef6
feroline <anacarolina.rrocha@gmail.com>
2024-06-26 16:53:56 -0300

    escrevendo o primeiro teste

--- cypress/e2e/add.cy.js
+++ cypress/e2e/add.cy.js
@@ -1,5 +1,10 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Lista TODO', () => {]
+  
+// TODO:Refatorar 
+  it('Deve guardar o valor de um input como texto', () => {
+    cy.visit('https://localhost:3000/')
+    
+    cy.get('[data-testid="input-todo"]').type('Primeiro item TODO')
+    cy.get('[data-testid="input-todo"]').should('have.value', 'Primeiro item TODO')
   })
 })
